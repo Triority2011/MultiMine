8830987f7e7a5b326736843bf7bdba9b61ea2e38
Trio <96701261+Triority2011@users.noreply.github.com>
2021-12-27 15:25:59 +0100

    Start.cmd now automatically gets loaded
    After execution the pmmp start.cmd is being added to your file system (MultiMine folder)

--- launcher.bat
+++ launcher.bat
@@ -12,7 +12,8 @@ goto Setup
 :Setup
 echo Generating necessary Files...
 
-echo %DESKTOP%
+:echo %DESKTOP%
+
 
 cd %DESKTOP%
 mkdir MultiMine
@@ -20,6 +21,9 @@ cd %DESKTOP%\MultiMine
 mkdir PMMP_Installations_Here
 mkdir MultiMine_Servers
 
+
+curl -o start.cmd https://raw.githubusercontent.com/pmmp/PocketMine-MP/stable/start.cmd
+:curl -o bin.zip https://jenkins.pmmp.io/job/PHP-8.0-Aggregate/lastSuccessfulBuild/artifact/PHP-8.0-Windows-x64.zip
 
 echo Please place your desired pocketmine installation inside %DESKTOP%\MultiMine\PMMP_Installations_Here, before starting this program!
 
@@ -40,10 +44,48 @@ goto Launch
 
 
 :Launch
+
+set /p prmt_name=Would you like to name each server you create individually? (yes=y no=n):
+if %prmt_name%==y (goto Name_y) else (goto Name_n)
+
+
+:Name_y
 set /p srvs_amnt=How many servers would you like? (you will have to name each):
+goto Generate
+
+:Name_n
+set /p srvs_amnt=How many servers would you like? (the names will be auto generated):
+goto Generate
 
 
 
+
+
+:Generate
+echo The Server folders will now be generated, press enter to continune!
+pause
+echo Generating Files...
+
+
+
+set loop=0
+set loop_tar=%srvs_amnt%
+:loop
+echo hello world
+mkdir Server(%loop%)
+
+xcopy "%DESKTOP%\MultiMine\PMMP_Installations_Here\PocketMine-MP.phar" "%DESKTOP%\MultiMine\MultiMine_Servers\Server(%loop%)" /z /i
+
+set /a loop=%loop%+1 
+if "%loop%"=="%loop_tar%" goto next
+goto loop
+
+:next
+
+
+echo This text will appear after repeating "hello world" for 2 times.
+
+pause
 
 
 
